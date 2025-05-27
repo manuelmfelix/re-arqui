5b57820016ac84315f23fc5f3f1e2b3ecfb1378d
Manuel Félix <manuel@manuelfelix.eu>
2025-05-27 22:11:53 +0100

    new

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,11 +2,6 @@ import imp
 import os
 import sys
 
-# Add the project directory to the path
-INTERP = os.path.expanduser("/home/rearquip/virtualenv/repositories/rearqui/3.11/bin/python")
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-
 sys.path.insert(0, os.path.dirname(__file__))
 
 wsgi = imp.load_source('wsgi', 're_arqui/wsgi.py')
