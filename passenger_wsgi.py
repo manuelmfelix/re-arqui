22e6902ce3ba30759282333686918f4cba1bd309
Manuel Félix <manuel@manuelfelix.eu>
2025-05-27 22:04:55 +0100

    passsenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,13 @@
+import imp
 import os
 import sys
 
 # Add the project directory to the path
+INTERP = os.path.expanduser("/home/rearquip/virtualenv/repositories/rearqui/3.11/bin/python")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Set up the Django settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "re_arqui.settings_prod")
-
-# Import the WSGI application
-from re_arqui.wsgi import application
+wsgi = imp.load_source('wsgi', 're_arqui/wsgi.py')
+application = wsgi.application
